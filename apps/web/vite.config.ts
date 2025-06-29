3c954e534d7fdb25a90bce8e28f8e2fa3006adc2
vishnu-cw <vishnu@codewalnut.com>
2025-06-29 16:08:38 +0000

    chore: update Vite configuration to load environment variables and enhance API proxy settings

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,17 +1,27 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
 
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: process.env.VITE_API_URL,
+	return {
+		plugins: [reactRouter(), tailwindcss(), tsconfigPaths()],
+
+		preview: {
+			port: 3000,
+		},
+
+		server: {
+			port: 3000,
+			proxy: {
+				"/api": {
+					changeOrigin: true,
+					target: env.VITE_API_URL,
+					rewrite: (path) => path.replace(/^\/api/, ""),
+				},
 			},
 		},
-	},
+	};
 });
